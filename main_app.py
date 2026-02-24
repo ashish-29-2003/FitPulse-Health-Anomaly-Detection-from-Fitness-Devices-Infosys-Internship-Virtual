9d8f52858caa38ad49972c55dab43b2f5c6bcb0c
ashish-29-2003 <kushwahaashish685@gmail.com>
2026-02-24 19:48:03 +0530

    ui is final

--- main_app.py
+++ main_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
-from sklearn.ensemble import IsolationForest  # Unique addition for high marks
+
 
 # Page configuration
 st.set_page_config(
